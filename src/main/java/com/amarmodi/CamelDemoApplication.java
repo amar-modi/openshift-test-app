119a1fac0b96490f2e1110154fbe756cfd76e28b
Your Name <you@example.com>
2019-04-26 10:46:25 -0400

    init commit

---
+++ src/main/java/com/amarmodi/CamelDemoApplication.java
@@ -0,0 +1,17 @@
+package com.amarmodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelDemoApplication {
+
+    // TODO Fuse
+    // should know JBoss WAR Fuse
+    // JMS WebLogic --> ActiveMQ
+    // Cloud based --> Start activeMQ
+    public static void main(String[] args) {
+        SpringApplication.run(CamelDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/amarmodi/config/Bootstrap.java
@@ -0,0 +1,25 @@
+package com.amarmodi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+public class Bootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    private DataSource dataSource;
+
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        jdbcTemplate.execute("Create table if not exists INPUT_POSTS_FAKE ( id numeric ,name varchar(255));");
+        jdbcTemplate.execute("Create table if not exists COUNTRY_FAKE (country_i serial,name text, country_code text, population numeric, crte_ts timestamp default now());");
+    }
+}
