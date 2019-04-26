fd768b13876304ceffd56685b2942b6aec5df5c5
Your Name <you@example.com>
2019-04-26 15:31:40 -0400

    adding tcp connection to amq

--- src/main/java/com/amarmodi/config/mongodb/ActiveMQConfiguration.java
+++ src/main/java/com/amarmodi/config/mongodb/ActiveMQConfiguration.java
@@ -29,8 +29,8 @@ public class ActiveMQConfiguration {
     public ActiveMQConnectionFactory activeMQConnectionFactory(){
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
         activeMQConnectionFactory.setBrokerURL(environment.getProperty("spring.activemq.brokerUrl"));
-        activeMQConnectionFactory.setPassword(environment.getProperty("spring.activemq.broker.password"));
-        activeMQConnectionFactory.setUserName(environment.getProperty("spring.activemq.broker.username"));
+        activeMQConnectionFactory.setPassword(environment.getProperty("spring.activemq.password"));
+        activeMQConnectionFactory.setUserName(environment.getProperty("spring.activemq.user"));
         activeMQConnectionFactory.setTrustAllPackages(true);
         return activeMQConnectionFactory;
     }
